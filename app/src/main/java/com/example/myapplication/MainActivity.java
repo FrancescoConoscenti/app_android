7aab816e948dd583fa93a15b2e10907d33a07cb8
cono <gianfranco.conoscenti@iticopernico.it>
2021-12-12 23:17:41 +0100

    provaj2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,6 +12,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -25,14 +27,21 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
      String url;
+     String [] inizio;
+     String [] orario;
+     TextView tw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inizio=new String [200];
+        orario=new String [200];
+        url="https://corsi.unibo.it/laurea/ElettronicaTelecomunicazioni/orario-lezioni/@@orario_reale_json?anno=3&curricula=995-000";
+        tw=(TextView) findViewById(R.id.textView);
     }
 
     public void open_menu(View view) {
-         url="https://corsi.unibo.it/laurea/ElettronicaTelecomunicazioni/orario-lezioni/@@orario_reale_json?anno=3&curricula=995-000";
+
         open_request();
         Intent intent=new Intent(this,MenuActivity.class);
         startActivity(intent);
@@ -41,23 +50,48 @@ public class MainActivity extends AppCompatActivity {
     public void open_request() {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        S stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, null,
+               new Response.Listener<JSONArray>() {
+                   @Override
+                   public void onResponse(JSONArray response) {
+                       try{
+                       for (int i=0;i<response.length();i++)
+                       {
+
+
+                           JSONObject datoO=response.getJSONObject(i);
+
+                            inizio[i]= datoO.getString("start");
+                            orario[i]= datoO.getString("time");
+                            tw.append(inizio[i]);
+
+                       }
+                       } catch (JSONException e) {
+                           e.printStackTrace();
+                       }
+                   }
+               }
+               // new Response.Listener<JSONArray>()
+               /*{
+                   @Override
+                   public void onResponse(JSONArray response) {
                         try {
-                            save_json(response);
-                            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-                        } catch (IOException e) {
+                            for (int i=0;i<response.length();i++)
+                            {
+
+                            }
+                        } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            }
-        });
-        queue.add(stringRequest);
+                }*/, new Response.ErrorListener() {
+           @Override
+           public void onErrorResponse(VolleyError error) {
+
+
+           }
+       });
+        queue.add(jsonArrayRequest);
     }
     private void save_json(String http) throws IOException {
         //inizializzo il file
